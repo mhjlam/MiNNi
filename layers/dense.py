01def1da78ce3740d2f4a723043738826b7d824d
Maurits Lam <mhjlam@gmail.com>
2024-01-17 15:21:34 +0100

    Improve distinction between generic and dense layers, and add gitignore properly

--- layers/dense.py
+++ layers/dense.py
@@ -1,3 +1,4 @@
+import numpy
 from .layer import Layer
 
 '''
@@ -5,14 +6,24 @@ This layer class is also called fully-connected or fc for short.
 '''
 
 class Dense(Layer):
-    def __init__(self, n_inputs, n_neurons):
-        Layer.__init__(self, n_inputs, n_neurons)
-
+    def __init__(self, inputs, n_neurons):
+      self.type = 'Dense'
+      self.inputs = inputs
+      self.weights = 0.01 * numpy.random.randn(numpy.shape(inputs)[1], n_neurons)
+      self.biases = numpy.zeros((1, n_neurons))
+   
     def __str__(self, n=0):
-        return Layer.__str__(self, n)
-
+      n = 1 if n < 1 else n
+      return f'Layer {str(n)}:\n  type: {self.type}\n\n' + \
+             f'  Inputs:\n' + \
+             f'    {str(self.inputs)}' + \
+             f'  Weights:\n' + \
+             f'    {str(self.weights)}' + \
+             f'  Biases:\n' + \
+             f'    {str(self.biases)}'
+             
     def str(self, n=0):
         return Layer.str(self, n)
 
-    def forward(self, inputs):
-        return Layer.forward(self, inputs)
+    def forward(self):
+        return Layer.forward(self, self.inputs)
--- layers/layer.py
+++ layers/layer.py
@@ -1,15 +1,14 @@
 import numpy
 
 class Layer:
-   def __init__(self, n_inputs, n_neurons):
-      self.weights = 0.01 * numpy.random.randn(n_inputs, n_neurons)
-      self.biases = numpy.zeros((1, n_neurons))
+   def __init__(self, neurons):
+      self.neurons = neurons
    
    def __str__(self, n=0):
       label = f'Layer {str(n)}:\n' if n >= 0 else 'Layer:\n'
       return label + '  type: Generic\n\n' + \
-         '\n'.join(['  ' + '  '.join(n.str(i).splitlines(True)) 
-                    for i, n in enumerate(self.neurons)])
+                     '\n'.join(['  ' + '  '.join(n.str(i).splitlines(True)) 
+                                for i, n in enumerate(self.neurons)])
 
    def str(self, n=0):
       try:
